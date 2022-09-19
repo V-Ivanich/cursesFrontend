3979c689f9ad7a7ea3a373d8e17657d9d81aa468
V-Ivanich <chipai_dom@mail.ru>
2022-09-19 09:17:02 +0300

    add contect this

---
+++ M1_Block_12_This/task_1.js
@@ -0,0 +1,33 @@
+const student = {
+  stack: ['HTML'],
+  level: 1,
+  improveLevel() {
+    this.level += 1
+    console.log(this)
+    switch (this.level) {
+      case 2:
+        this.stack.push('CSS')
+        break
+      case 3:
+        this.stack.push('JavaScript')
+        break
+      case 4:
+        this.stack.push('React')
+        break
+      case 5:
+        this.stack.push('NodeJS')
+        break
+      default:
+        alert('Студент выучил все технологии!')
+        break
+    }
+    return this
+  },
+}
+
+student
+  .improveLevel()
+  .improveLevel()
+  .improveLevel()
+  .improveLevel()
+  .improveLevel()
