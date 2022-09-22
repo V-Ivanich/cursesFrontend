14a96784afadb9e070c7472f7758371c86be3b1f
V-Ivanich <chipai_dom@mail.ru>
2022-09-22 15:21:25 +0300

    dop taska

--- M1_Block_13_Class_OOP/task_3.js
+++ M1_Block_13_Class_OOP/task_3.js
@@ -63,3 +63,44 @@ dictionary.showAllWords()
 // Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает
 // и дополняет существующие
 console.log(dictionary)
+//*---answer
+class Dictionary {
+  constructor(name) {
+    this.name = name
+    this.words = {}
+  }
+
+  add(word, description) {
+    if (!this.words[word]) {
+      this.words[word] = {
+        word,
+        description,
+      }
+    }
+  }
+
+  remove(word) {
+    delete this.words[word]
+  }
+
+  get(word) {
+    return this.words[word]
+  }
+
+  showAllWords() {
+    Object.values(this.words).forEach((wordItem) => {
+      console.log(`${wordItem.word} - ${wordItem.description}`)
+    })
+  }
+}
+
+const dictionary = new Dictionary('Толковый словарь')
+dictionary.add('JavaScript', 'популярный язык программирования')
+dictionary.add(
+  'Веб-разработчик',
+  'Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие'
+)
+
+dictionary.remove('JavaScript')
+dictionary.showAllWords()
+// Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие
--- доп_4/task-1.js
+++ доп_4/task-1.js
@@ -1,32 +1,48 @@
-const attacker = {
-  archer: 30,
-  footSoldier: 55,
-  cavalry: 10,
-  artillery: 3,
-  checkChancesToWin(defenderObject) {
-    let chance = 0
-    for (let key in defenderObject) {
-      console.log(key)
-    }
-  },
-  improveArmy() {
-    for (let key in this) {
-      key += 5
-    }
-    // this.archer += 5
-    // this.footSoldier += 5
-    // this.cavalry += 5
-    // this.artillery += 5
-  },
-  attack(defender) {},
-}
-attacker.improveArmy()
-
 const defender = {
   archer: 33,
   footSoldier: 50,
   cavalry: 40,
   artillery: 10,
 }
-attacker.checkChancesToWin(defender)
-console.log()
+
+const attacker = {
+  archer: 30,
+  footSoldier: 55,
+  cavalry: 10,
+  artillery: 3,
+
+  checkChancesToWin: function (defenderObject) {
+    let chance = 0
+    const keysDefender = Object.getOwnPropertyNames(defenderObject)
+    keysDefender.forEach((elem) => {
+      if (defenderObject[elem] < this[elem]) chance++
+    })
+    return [chance, keysDefender.length]
+  },
+
+  improveArmy: function () {
+    const keysAttacker = Object.getOwnPropertyNames(this)
+    keysAttacker.forEach((elem) => {
+      this[elem] += 5
+    })
+  },
+
+  attack: function (defender) {
+    const countChance = this.checkChancesToWin(defender)
+    if ((countChance[0] * 100) / countChance[1] > 70) {
+      console.log('Мы усилились! Мы несомненно победим! Наши шансы высоки!')
+    } else {
+      console.log(
+        `Наши шансы равны ${countChance[0]}/${countChance[1]}. Необходимо укрепление!`
+      )
+      this.improveArmy()
+    }
+  },
+}
+attacker.attack(defender)
+attacker.attack(defender)
+
+attacker.attack(defender)
+// attacker.attack(defender) // Наши шансы равны 1/4. Необходимо укрепление!
+// attacker.attack(defender) // Наши шансы равны 2/4. Необходимо укрепление!
+// attacker.attack(defender) // Мы усилились! Мы несомненно победим! Наши шансы высоки!
