107174a95b576dc4b3e13e717111b2196a317a1a
V-Ivanich <chipai_dom@mail.ru>
2022-09-26 18:05:49 +0300

    fix dop

--- доп_4/task-1.js
+++ доп_4/task-1.js
@@ -45,3 +45,61 @@ const attacker = {
 attacker.attack(defender) //? Наши шансы равны 1/4. Необходимо укрепление!
 attacker.attack(defender) //? Наши шансы равны 2/4. Необходимо укрепление!
 attacker.attack(defender) //? Мы усилились! Мы несомненно победим! Наши шансы высоки!
+
+//* ----answer
+const attacker = {
+  archer: 30,
+  footSoldier: 55,
+  cavalry: 10,
+  artillery: 3,
+  checkChancesToWin(defenderObject) {
+    const attackerValues = Object.values(this)
+    const defenderValues = Object.values(defenderObject)
+    let chancesCounter = 0
+    attackerValues.forEach((value, index) => {
+      if (value > defenderValues[index] && typeof value === 'number') {
+        chancesCounter += 1
+      }
+    })
+
+    return [chancesCounter, defenderValues.length]
+  },
+  improveArmy() {
+    Object.entries(this).forEach(item => {
+      const key = item[0]
+      const value = item[1]
+      if (typeof value === 'number') {
+        this[key] = value + 5
+      }
+    })
+  },
+  attack(defender) {
+    const chancesValues = this.checkChancesToWin(defender)
+    console.log('chancesValues', chancesValues)
+    const ourArmyChances = chancesValues[0]
+    const seventyPercentChances = Math.round((chancesValues[1] * 70) / 100)
+    console.log('ourArmyChances', ourArmyChances)
+    console.log('seventyPercentChances', seventyPercentChances)
+    if (ourArmyChances < seventyPercentChances) {
+      alert(
+        `Наши шансы равны ${ourArmyChances}/${chancesValues[1]}. Необходимо укрепление!`,
+      )
+      this.improveArmy()
+    } else {
+      alert('Мы усилились! Мы несомненно победим! Наши шансы высоки!')
+    }
+
+    console.log(this)
+  },
+}
+
+const defender = {
+  archer: 33,
+  footSoldier: 50,
+  cavalry: 40,
+  artillery: 10,
+}
+
+attacker.attack(defender) // Наши шансы равны 1/4. Необходимо укрепление!
+attacker.attack(defender) // Наши шансы равны 2/4. Необходимо укрепление!
+attacker.attack(defender) // Мы усилились! Мы несомненно победим! Наши шансы высоки!
--- доп_4/task-3.js
+++ доп_4/task-3.js
@@ -49,3 +49,43 @@ const carService = new CarService('RepairCarNow', {
   till: '20:00',
 })
 carService.repairCar('BMW')
+
+//* ----answer
+class CarService {
+  static DefaultWorkingHours = {
+    from: '9:00',
+    till: '20:00',
+  }
+
+  constructor(name, workingHours) {
+    this.name = name
+    this.workingHours = workingHours || CarService.DefaultWorkingHours
+  }
+
+  repairCar(carName) {
+    if (!carName) {
+      console.error(
+        'Вам необходимо указать название машины, чтобы ее отремонтировать',
+      )
+    } else {
+      const currentTime = new Date()
+      const currentHours = currentTime.getHours()
+      const workingHoursFrom = Number(this.workingHours.from.split(':')[0])
+      const workingHoursTill = Number(this.workingHours.till.split(':')[0])
+
+      if (currentHours >= workingHoursFrom && currentHours < workingHoursTill) {
+        alert(
+          `Сейчас отремонтируем вашу машину ${carName}! Ожидайте, пожалуйста.`,
+        )
+      } else {
+        alert('К сожалению, мы сейчас закрыты. Приходите завтра.')
+      }
+    }
+  }
+}
+
+const carService = new CarService('RepairCarNow', {
+  from: '8:00',
+  till: '20:00',
+})
+carService.repairCar('BMW')
