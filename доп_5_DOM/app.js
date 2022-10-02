c4af507a0969285c8f3deb041dc3351318f33720
V-Ivanich <chipai_dom@mail.ru>
2022-10-02 22:04:47 +0300

    write dop-5 dom2

--- доп_5_DOM/app.js
+++ доп_5_DOM/app.js
@@ -1,9 +1,29 @@
+// const buttonSelect = document.querySelector('.select-dropdown__button')
+// const ulSelect = document.querySelector('.select-dropdown__list')
+// buttonSelect.addEventListener('click', () => {
+//   ulSelect.classList.add('active')
+// })
+const container = document.querySelector('#container')
+
 class CustomSelect {
   constructor(id, options) {
     this.id = id
     this.options = options
   }
-  render() {}
+
+  createContainer() {
+    const divContainer = document.createElement('div')
+    divContainer.classList.add('select-dropdown', `select-dropdown--${this.id}`)
+    container.append(divContainer)
+
+    const buttonSelected = document.createElement('button')
+    buttonSelected.classList.add(
+      'select-dropdown__button',
+      `select-dropdown__button--${this.id}`,
+    )
+  }
+
+  render(container) {}
 }
 const options = [
   { value: 1, text: 'JavaScript' },
@@ -14,7 +34,7 @@ const options = [
 ]
 
 const custom2 = new CustomSelect(123, options)
-
+custom2.createContainer()
 console.log(custom2)
 
 // const customSelect = new CustomSelect('123', options)
