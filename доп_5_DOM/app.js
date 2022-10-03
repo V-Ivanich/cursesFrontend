1537487f4aa183d3258fd30314dd34a4dc278238
V-Ivanich <chipai_dom@mail.ru>
2022-10-03 13:00:07 +0300

    add plagins in task-dop5

--- доп_5_DOM/app.js
+++ доп_5_DOM/app.js
@@ -3,24 +3,75 @@
 // buttonSelect.addEventListener('click', () => {
 //   ulSelect.classList.add('active')
 // })
-const container = document.querySelector('#container')
+const mainContainer = document.querySelector('#container')
 
 class CustomSelect {
   constructor(id, options) {
     this.id = id
     this.options = options
+    this.currentSelectedOption
   }
 
   createContainer() {
     const divContainer = document.createElement('div')
     divContainer.classList.add('select-dropdown', `select-dropdown--${this.id}`)
-    container.append(divContainer)
+    mainContainer.append(divContainer)
 
     const buttonSelected = document.createElement('button')
     buttonSelected.classList.add(
       'select-dropdown__button',
       `select-dropdown__button--${this.id}`,
     )
+    divContainer.prepend(buttonSelected)
+
+    const spanSelected = document.createElement('span')
+    spanSelected.classList.add('select-dropdown', `select-dropdown--${this.id}`)
+    spanSelected.textContent = 'Выберите элемент'
+    buttonSelected.append(spanSelected)
+
+    const ulList = document.createElement('ul')
+    ulList.classList.add(
+      'select-dropdown__list',
+      `select-dropdown__list${this.id}`,
+    )
+    divContainer.append(ulList)
+
+    this.options.forEach(element => {
+      let liSelected = document.createElement('li')
+      liSelected.classList.add('select-dropdown__list-item')
+      liSelected.setAttribute('data-value', element.value)
+      liSelected.textContent = element.text
+      ulList.append(liSelected)
+    })
+
+    const buttonSelect = document.querySelector('.select-dropdown__button')
+    const ulSelect = document.querySelector('.select-dropdown__list')
+    buttonSelect.addEventListener('click', () => {
+      ulSelect.classList.add('active')
+    })
+    this.liListener(ulList)
+  }
+
+  liListener(listUl) {
+    listUl.addEventListener('click', e => {
+      const liTarget = e.target.closest('li')
+      if (liTarget) {
+        const allLi = document.querySelectorAll('.select-dropdown__list-item')
+        allLi.forEach(elem => {
+          elem.classList.remove('selected')
+        })
+        liTarget.classList.add('selected')
+        this.options.forEach(item => {
+          if (item.value === +liTarget.dataset.value) {
+            this.currentSelectedOption = item
+          }
+        })
+        document
+          .querySelector('.select-dropdown__list')
+          .classList.remove('active')
+        console.log(this.currentSelectedOption)
+      }
+    })
   }
 
   render(container) {}
@@ -33,10 +84,8 @@ const options = [
   { value: 5, text: 'CSS' },
 ]
 
-const custom2 = new CustomSelect(123, options)
-custom2.createContainer()
-console.log(custom2)
+const customSelect = new CustomSelect('123', options)
+customSelect.createContainer()
+console.log(customSelect)
 
-// const customSelect = new CustomSelect('123', options)
-// const mainContainer = document.querySelector('#container')
 // customSelect.render(mainContainer)
