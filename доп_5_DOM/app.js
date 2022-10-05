7cbc066aa424fceb326f16a4dc3a3fc1a0e62037
V-Ivanich <chipai_dom@mail.ru>
2022-10-05 13:27:52 +0300

    qustions

--- доп_5_DOM/app.js
+++ доп_5_DOM/app.js
@@ -1,139 +1,134 @@
-// const buttonSelect = document.querySelector('.select-dropdown__button')
-// const ulSelect = document.querySelector('.select-dropdown__list')
-// buttonSelect.addEventListener('click', () => {
-//   ulSelect.classList.add('active')
-// })
-const mainContainer = document.querySelector('#container')
+// const mainContainer = document.querySelector('#container')
 
-class CustomSelect {
-  #id
-  #options
-  #currentSelectedOption
-  constructor(id, options) {
-    this.#id = id
-    this.#options = options
-    this.#currentSelectedOption
-  }
+// class CustomSelect {
+//   #id
+//   #options
+//   #currentSelectedOption
+//   constructor(id, options) {
+//     this.#id = id
+//     this.#options = options
+//     this.#currentSelectedOption
+//   }
 
-  #createContainer(container) {
-    const divContainer = document.createElement('div')
-    divContainer.classList.add(
-      'select-dropdown',
-      `select-dropdown--${this.#id}`,
-    )
-    container.append(divContainer)
+//   #createContainer(container) {
+//     const divContainer = document.createElement('div')
+//     divContainer.classList.add(
+//       'select-dropdown',
+//       `select-dropdown--${this.#id}`,
+//     )
+//     container.append(divContainer)
 
-    const divPrompt = document.createElement('div')
-    divPrompt.classList.add('select-prompt')
-    divPrompt.textContent = 'Test string'
-    divContainer.append(divPrompt)
+//     const divPrompt = document.createElement('div')
+//     divPrompt.classList.add('select-prompt')
+//     divPrompt.textContent = 'Test string'
+//     divContainer.append(divPrompt)
 
-    const buttonSelected = document.createElement('button')
-    buttonSelected.classList.add(
-      'select-dropdown__button',
-      `select-dropdown__button--${this.#id}`,
-    )
-    divContainer.prepend(buttonSelected)
+//     const buttonSelected = document.createElement('button')
+//     buttonSelected.classList.add(
+//       'select-dropdown__button',
+//       `select-dropdown__button--${this.#id}`,
+//     )
+//     divContainer.prepend(buttonSelected)
 
-    const spanSelected = document.createElement('span')
-    spanSelected.classList.add(
-      'select-dropdown',
-      `select-dropdown--${this.#id}`,
-    )
-    spanSelected.textContent = 'Выберите элемент'
-    buttonSelected.append(spanSelected)
+//     const spanSelected = document.createElement('span')
+//     spanSelected.classList.add(
+//       'select-dropdown',
+//       `select-dropdown--${this.#id}`,
+//     )
+//     spanSelected.textContent = 'Выберите элемент'
+//     buttonSelected.append(spanSelected)
 
-    const ulList = document.createElement('ul')
-    ulList.classList.add(
-      'select-dropdown__list',
-      `select-dropdown__list${this.#id}`,
-    )
-    divContainer.append(ulList)
+//     const ulList = document.createElement('ul')
+//     ulList.classList.add(
+//       'select-dropdown__list',
+//       `select-dropdown__list${this.#id}`,
+//     )
+//     divContainer.append(ulList)
 
-    this.#options.forEach(element => {
-      let liSelected = document.createElement('li')
-      liSelected.classList.add('select-dropdown__list-item')
-      liSelected.setAttribute('data-value', element.value)
-      liSelected.textContent = element.text
-      ulList.append(liSelected)
-    })
+//     this.#options.forEach(element => {
+//       let liSelected = document.createElement('li')
+//       liSelected.classList.add('select-dropdown__list-item')
+//       liSelected.setAttribute('data-value', element.value)
+//       liSelected.textContent = element.text
+//       ulList.append(liSelected)
+//     })
 
-    const buttonSelect = document.querySelector('.select-dropdown__button')
-    const ulSelect = document.querySelector('.select-dropdown__list')
-    buttonSelect.addEventListener('click', () => {
-      ulSelect.classList.add('active')
-    })
-    this.#liListener()
-    this.#moveCursor()
-  }
+//     const buttonSelect = document.querySelector('.select-dropdown__button')
+//     const ulSelect = document.querySelector('.select-dropdown__list')
+//     buttonSelect.addEventListener('click', () => {
+//       ulSelect.classList.add('active')
+//     })
+//     this.#liListener()
+//     this.#moveCursor()
+//   }
 
-  #moveCursor() {
-    document
-      .querySelector('.select-dropdown__button')
-      .addEventListener('mouseover', () => {
-        if (this.#currentSelectedOption) {
-          document
-            .querySelector('.select-prompt')
-            .classList.add('active-prompt')
-        }
-      })
+//   #moveCursor() {
+//     document
+//       .querySelector('.select-dropdown__button')
+//       .addEventListener('mouseover', () => {
+//         if (this.#currentSelectedOption) {
+//           document
+//             .querySelector('.select-prompt')
+//             .classList.add('active-prompt')
+//         }
+//       })
 
-    document
-      .querySelector('.select-dropdown__button')
-      .addEventListener('mouseout', () => {
-        document
-          .querySelector('.select-prompt')
-          .classList.remove('active-prompt')
-      })
-  }
+//     document
+//       .querySelector('.select-dropdown__button')
+//       .addEventListener('mouseout', () => {
+//         document
+//           .querySelector('.select-prompt')
+//           .classList.remove('active-prompt')
+//       })
+//   }
 
-  #liListener() {
-    document.querySelector('body').addEventListener('click', e => {
-      const liTarget = e.target.closest('li')
+//   #liListener() {
+//     document.querySelector('body').addEventListener('click', e => {
+//       const liTarget = e.target.closest('li')
 
-      if (e.target.tagName === 'BODY') {
-        document
-          .querySelector('.select-dropdown__list')
-          .classList.remove('active')
-      }
-      if (liTarget && !liTarget.classList.contains('selected')) {
-        const allLi = document.querySelectorAll('.select-dropdown__list-item')
-        allLi.forEach(elem => {
-          elem.classList.remove('selected')
-        })
-        liTarget.classList.add('selected')
-        this.#options.forEach(item => {
-          if (item.value === +liTarget.dataset.value) {
-            this.#currentSelectedOption = item
-            document.querySelector('.select-prompt').textContent = item.text
-          }
-        })
-        document
-          .querySelector('.select-dropdown__list')
-          .classList.remove('active')
-      }
-    })
-  }
+//       if (e.target.tagName === 'BODY') {
+//         document
+//           .querySelector('.select-dropdown__list')
+//           .classList.remove('active')
+//       }
+//       if (liTarget && !liTarget.classList.contains('selected')) {
+//         const allLi = document.querySelectorAll('.select-dropdown__list-item')
+//         allLi.forEach(elem => {
+//           elem.classList.remove('selected')
+//         })
+//         liTarget.classList.add('selected')
+//         this.#options.forEach(item => {
+//           if (item.value === +liTarget.dataset.value) {
+//             this.#currentSelectedOption = item
+//             document.querySelector('.select-prompt').textContent = item.text
+//           }
+//         })
+//         document
+//           .querySelector('.select-dropdown__list')
+//           .classList.remove('active')
+//       }
+//     })
+//   }
 
-  get selectedValue() {
-    return this.#currentSelectedOption
-  }
+//   get selectedValue() {
+//     return this.#currentSelectedOption
+//   }
 
-  render(container) {
-    this.#createContainer(container)
-  }
-}
-const options = [
-  { value: 1, text: 'JavaScript' },
-  { value: 2, text: 'NodeJS' },
-  { value: 3, text: 'ReactJS' },
-  { value: 4, text: 'HTML' },
-  { value: 5, text: 'CSS' },
-]
+//   render(container) {
+//     this.#createContainer(container)
+//   }
+// }
+// const options = [
+//   { value: 1, text: 'JavaScript' },
+//   { value: 2, text: 'NodeJS' },
+//   { value: 3, text: 'ReactJS' },
+//   { value: 4, text: 'HTML' },
+//   { value: 5, text: 'CSS' },
+// ]
 
-const customSelect = new CustomSelect('123', options)
-customSelect.render(mainContainer)
-console.log(customSelect.selectedValue)
+// const customSelect = new CustomSelect('123', options)
+// customSelect.render(mainContainer)
+// console.log(customSelect.selectedValue)
 
 //*----answer
 class CustomSelect {
@@ -255,3 +250,6 @@ const options = [
 const customSelect = new CustomSelect('123', options)
 const mainContainer = document.querySelector('#container')
 customSelect.render(mainContainer)
+
+//? ---Вопросы
+// по поводу bind и addEventListener у элементов li
