ff5adfd40177d681cf3b4001469f7bcd6d1c03ee
V-Ivanich <chipai_dom@mail.ru>
2022-10-13 20:27:45 +0300

    fix all problems

--- доп_6_Promise_async/task-1.js
+++ доп_6_Promise_async/task-1.js
@@ -21,101 +21,104 @@ class CreateElem {
   }
 }
 
-const renderPost = async postId => {
+const getInDataBase = async postId => {
   try {
     const dataPost = await fetch(`${postURL}/${postId}`)
     const dataComments = await fetch(`${commentsURL}${postId}`)
     const dataResult = await Promise.all([dataPost, dataComments])
     const parsingData = dataResult.map(responses => responses.json())
-    console.log(parsingData)
     const allTasks = await Promise.all(parsingData)
-    console.log(allTasks)
-    const titlePost = allTasks[0].title
-    const textPost = allTasks[0].body
-
-    const divsBody = new CreateElem([
-      'divBody',
-      'post',
-      'post',
-      'div',
-      '',
-      '.cont',
-    ])
-    divsBody.setNewElement()
-
-    const blockH1 = new CreateElem([
-      'blockH1',
-      '',
-      'post__title',
-      'h1',
-      titlePost,
-      '.post',
-    ])
-    blockH1.setNewElement()
-
-    const paragraf = new CreateElem([
-      'paragraf',
-      '',
-      'post__text',
-      'p',
-      textPost,
-      '.post',
-    ])
-    paragraf.setNewElement()
-
-    const textBold = new CreateElem([
-      'bold',
-      '',
-      'post__comments-text',
-      'b',
-      'Комментарии',
-      '.post',
-    ])
-    textBold.setNewElement()
-
-    const divComment = new CreateElem([
-      'divComment',
-      '',
-      'post__comments',
-      'div',
-      '',
-      '.post',
-    ])
-    divComment.setNewElement()
-
-    allTasks[1].forEach(elem => {
-      const divText = new CreateElem([
-        'divComment',
-        '',
-        'post-comments',
-        'div',
-        '',
-        '.post__comments',
-      ])
-      divText.setNewElement()
-
-      const spanAuthor = new CreateElem([
-        'spanAuthor',
-        '',
-        'post-comment__author',
-        'span',
-        elem.email,
-        '.post-comments',
-      ])
-      spanAuthor.setNewElement()
-
-      const spanText = new CreateElem([
-        'spanText',
-        '',
-        'post-comment__text',
-        'span',
-        elem.body,
-        '.post-comments',
-      ])
-      spanText.setNewElement()
-    })
+    return allTasks
   } catch (error) {
     console.log(error)
   }
+}
+
+const renderPost = async postId => {
+  const allTasks = await getInDataBase(postId)
+  const titlePost = allTasks[0].title
+  const textPost = allTasks[0].body
+
+  const divsBody = new CreateElem([
+    'divBody',
+    'post',
+    'post',
+    'div',
+    '',
+    '.cont',
+  ])
+  divsBody.setNewElement()
+
+  const blockH1 = new CreateElem([
+    'blockH1',
+    '',
+    'post__title',
+    'h1',
+    titlePost,
+    '.post',
+  ])
+  blockH1.setNewElement()
+
+  const paragraf = new CreateElem([
+    'paragraf',
+    '',
+    'post__text',
+    'p',
+    textPost,
+    '.post',
+  ])
+  paragraf.setNewElement()
+
+  const textBold = new CreateElem([
+    'bold',
+    '',
+    'post__comments-text',
+    'b',
+    'Комментарии',
+    '.post',
+  ])
+  textBold.setNewElement()
+
+  const divComment = new CreateElem([
+    'divComment',
+    '',
+    'post__comments',
+    'div',
+    '',
+    '.post',
+  ])
+  divComment.setNewElement()
+
+  await allTasks[1].forEach((elem, index) => {
+    const divText = new CreateElem([
+      'divComment',
+      `post-comments${index}`,
+      'post-comments',
+      'div',
+      '',
+      '.post__comments',
+    ])
+    divText.setNewElement()
+
+    const spanAuthor = new CreateElem([
+      'spanAuthor',
+      '',
+      'post-comment__author',
+      'span',
+      elem.email,
+      `#post-comments${index}`,
+    ])
+    spanAuthor.setNewElement()
+
+    const spanText = new CreateElem([
+      'spanText',
+      '',
+      'post-comment__text',
+      'span',
+      elem.body,
+      `#post-comments${index}`,
+    ])
+    spanText.setNewElement()
+  })
 }
 renderPost(1)
